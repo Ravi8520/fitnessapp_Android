f2dea212a9e11661ad3bb371fdcce2e3c9130044
Naimish <naimish.t@clientsdemosite.in>
2022-10-18 10:37:15 +0530

    first commit 15sep2022

--- app/src/main/java/com/tfb/fitnessapp/MainActivity.java
+++ app/src/main/java/com/tfb/fitnessapp/MainActivity.java
@@ -1,13 +1,21 @@
 package com.tfb.fitnessapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tfb.fitnessapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    MainActivity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        activity = this;
     }
 }
--- app/src/main/java/com/tfb/fitnessapp/SplashActivity.java
+++ app/src/main/java/com/tfb/fitnessapp/SplashActivity.java
@@ -1,14 +1,31 @@
 package com.tfb.fitnessapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.tfb.fitnessapp.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ActivitySplashBinding binding;
+    SplashActivity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        activity = this;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(activity, MainActivity.class));
+            }
+        }, 3000);
+
     }
 }
