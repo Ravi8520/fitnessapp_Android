7629efd7fd6660ed5add1dcf1639eed386476532
Freebird <81305380+freebird-app-unit@users.noreply.github.com>
2022-11-08 17:36:54 +0530

    commit 8-11-22 fitness

--- app/src/main/java/com/tfb/fitnessapp/activities/ChooseAvailabilityActivity.java
+++ app/src/main/java/com/tfb/fitnessapp/activities/ChooseAvailabilityActivity.java
@@ -1,9 +1,9 @@
 package com.tfb.fitnessapp.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.tfb.fitnessapp.R;
 import com.tfb.fitnessapp.databinding.ActivityChooseAvailabilityBinding;
@@ -14,6 +14,8 @@ public class ChooseAvailabilityActivity extends AppCompatActivity {
     ActivityChooseAvailabilityBinding binding;
     private ChooseAvailabilityActivity mContext;
 
+    private boolean flag = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +23,46 @@ public class ChooseAvailabilityActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
-
         binding.backarrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
+            }
+        });
+
+        binding.txtMonday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (flag) {
+
+                    flag = false;
+                    binding.txtMonday.setBackground(getResources().getDrawable(R.drawable.coustom_button_border));
+                    binding.txtMonday.setTextColor(getResources().getColor(R.color.black));
+                } else {
+                    binding.txtMonday.setBackground(getResources().getDrawable(R.drawable.custome_edittext));
+                    binding.txtMonday.setTextColor(getResources().getColor(R.color.white));
+                    flag = true;
+
+                }
+            }
+        });
+        binding.txtTuesday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (flag) {
+
+                    flag = false;
+                     binding.txtTuesday.setBackground(getResources().getDrawable(R.drawable.coustom_button_border));
+                     binding.txtTuesday.setTextColor(getResources().getColor(R.color.black));
+                } else {
+
+                     binding.txtTuesday.setBackground(getResources().getDrawable(R.drawable.custome_edittext));
+                     binding.txtTuesday.setTextColor(getResources().getColor(R.color.white));
+                    flag = true;
+
+                }
             }
         });
 
