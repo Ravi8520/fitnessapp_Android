712148585c81724e14f972c7255ccf65054118d2
Naimish <naimish.t@clientsdemosite.in>
2022-11-08 13:39:12 +0530

    login design done 08nov2022

--- app/src/main/java/com/tfb/fitnessapp/activities/MainActivity.java
+++ app/src/main/java/com/tfb/fitnessapp/activities/MainActivity.java
@@ -1,10 +1,13 @@
 package com.tfb.fitnessapp.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.tfb.fitnessapp.R;
 import com.tfb.fitnessapp.databinding.ActivityMainBinding;
@@ -34,5 +37,29 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(mContext, ForgetPasswordActivity.class));
             }
         });
+
+        binding.showPassBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowHidePass(view);
+            }
+        });
+    }
+
+    public void ShowHidePass(View view) {
+
+        if (view.getId() == R.id.show_pass_btn) {
+            if (binding.editPassword.getTransformationMethod().equals(PasswordTransformationMethod.getInstance())) {
+                ((ImageView) (view)).setImageResource(R.drawable.ic_password_visibility);
+                //Show Password
+                binding.editPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                binding.editPassword.setSelection(binding.editPassword.getText().length());
+            } else {
+                ((ImageView) (view)).setImageResource(R.drawable.ic_password_visibleoff);
+                //Hide Password
+                binding.editPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                binding.editPassword.setSelection(binding.editPassword.getText().length());
+            }
+        }
     }
 }
