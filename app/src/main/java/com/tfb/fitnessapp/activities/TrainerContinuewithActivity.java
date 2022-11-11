cd82df21df18b811d48a00f494549c4bec364444
Freebird <81305380+freebird-app-unit@users.noreply.github.com>
2022-11-11 18:25:33 +0530

    commit 11-11-22 fitnessapp

--- app/src/main/java/com/tfb/fitnessapp/activities/TrainerContinuewithActivity.java
+++ app/src/main/java/com/tfb/fitnessapp/activities/TrainerContinuewithActivity.java
@@ -21,7 +21,7 @@ public class TrainerContinuewithActivity extends AppCompatActivity {
         mContext = this;
 
 
-        binding.txtSignin.setOnClickListener(new View.OnClickListener() {
+        binding.txtSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(mContext, TrainerDetailsActivity.class));
